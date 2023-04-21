bcf711580a5ff8e06488d8d42158b09c41f55695
Brandon Nelson <machinecar998@gmail.com>
2023-04-21 18:49:09 -0500

    fixed search bar and code

--- ajax.js
+++ ajax.js
@@ -1,51 +1,41 @@
 $(document).ready(function () {
-    const searchTerm = '=' + $('#search');
-    const list = $('#list');
+$('#search').on('submit', function (event) {
+    event.preventDefault();
 
-    let endpoint = 'https://itunes.apple.com/search?';
+    const searchTerm = $('#searchTerm').val();
+    let endpoint = 'https://itunes.apple.com/search';
     let params = {
-        term: searchTerm
+        term: searchTerm,
+        filter: 'full',
+        limit: 36,
     }
+
     $.get(
         endpoint,
         params,
         function (data) {
             // this called in the future, when a response is retrieved
-            console.log('list', data);
-            searchTerm.addEventListener("input", (event) =>{
-                const searchString = event.target.value.toLowerCase();
-                const filteredData = data.filter((item) =>
-                item.toLowerCase().includes(searchString)
-                );
-                displayData(filteredData)
-            });
+            console.log('response,', data);
 
-            list.html('');
-            const displayData = (data) => {
-                const htmlString = data
-                    .map((item) => `<li class="list-group-item">${item}</li>`)
-                    .join("");
-                list.innerHTML = htmlString;
-            };
+            $('#results').html('');
 
             data.results.forEach(result => {
                 if (result.kind) {
-                    list.append(`<h3>${result.kind}</h3>`);
+                    $('#results').append(`<h3>${result.kind}</h3>`);
                 }
                 if (result.artistName) {
-                    list.append(`<h3>${result.artistName}</h3>`);
+                    $('#results').append(`<h3>${result.artistName}</h3>`);
                 }
-                if(result.collectionName) {
-                    list.append(`<h3>${result.collectionName}</h3>`);
+                if (result.collectionName) {
+                    $('#results').append(`<h3>${result.collectionName}</h3>`);
                 }
-                if(result.trackName) {
-                    list.append(`<h3>${result.trackName}</h3>`);
+                if (result.trackName) {
+                    $('#results').append(`<h3>${result.trackName}</h3>`);
                 }
-            })
-
+            });
         },
-        'json' // could also be html, xml, text
+        'json'
     );
-
-    console.log('done loading');
 });
+});
+
